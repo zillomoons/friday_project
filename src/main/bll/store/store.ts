2f98b2c7e7e01efe408f3e0b36dd0bb994a57c7b
zillomoons <dlzht73@ya.ru>
2022-01-10 18:04:00 +0300

    store

--- src/main/bll/store/store.ts
+++ src/main/bll/store/store.ts
@@ -1,9 +1,10 @@
-import {combineReducers, createStore} from "redux";
+import {applyMiddleware, combineReducers, createStore} from "redux";
 import {loginReducer} from "../reducers/login-reducer";
 import {registerReducer} from "../reducers/register-reducer";
 import {passRecoverReducer} from "../reducers/pass-recover-reducer";
 import {setPassReducer} from "../reducers/set-pass-reducer";
 import {profileReducer} from "../reducers/profile-reducer";
+import thunk from 'redux-thunk';
 
 const rootReducer = combineReducers({
     login: loginReducer,
@@ -13,7 +14,7 @@ const rootReducer = combineReducers({
     profile: profileReducer,
 })
 
-const store = createStore(rootReducer)
+const store = createStore(rootReducer, applyMiddleware(thunk))
 
 export default store;
 
