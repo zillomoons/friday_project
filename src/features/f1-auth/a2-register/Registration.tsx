d7a232adf4757bc927a6854e11c95eadae4bdac7
zillomoons <dlzht73@ya.ru>
2022-01-22 14:14:48 +0300

    added styles

--- src/features/f1-auth/a2-register/Registration.tsx
+++ src/features/f1-auth/a2-register/Registration.tsx
@@ -1,11 +1,14 @@
+import SuperButton from "../../../main/ui/common/superButton/SuperButton"
 import SuperInputText from "../../../main/ui/common/superInput/SuperInput"
+import s from "./Registration.module.css";
 
 export const Registration = () => {
     return (
-        <div>
+        <div className={s.registerForm}>
             <h3>Registration</h3>
             <SuperInputText />
             <SuperInputText />
+            <SuperButton>Submit</SuperButton>
         </div>
     ) 
     
